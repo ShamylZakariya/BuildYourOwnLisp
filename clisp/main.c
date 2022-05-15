baa3ec47254765ed04daa1b69b2aecd88bfed89b
Shamyl Zakariya <shamyl@gmail.com>
2022-05-14 18:11:04 -0700

    Add libedit dependency and make repl better

--- clisp/main.c
+++ clisp/main.c
@@ -1,10 +1,21 @@
+#include <editline/readline.h>
 #include <stdio.h>
+#include <stdlib.h>
 
 #include "libclisp/libclisp.h"
 
 int main(int argc, char** argv)
 {
-    puts("Hello, world!");
-    test_func(111);
+    puts("Lispy Version 0.0.0.0.1");
+    puts("Press Ctrl+c to Exit\n");
+
+    while (1) {
+        char* input = readline("lispy >");
+        add_history(input);
+
+        printf("Echo: %s\n", input);
+        free(input);
+    }
+
     return 0;
 }
